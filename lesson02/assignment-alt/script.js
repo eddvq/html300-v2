d7250cffd3f1c044a00d006615e2a6541c16f94b
eddy vazquez <>
2021-04-24 12:20:25 -0700

    Able to create one card however other cards are not rendering

--- lesson02/assignment-alt/script.js
+++ lesson02/assignment-alt/script.js
@@ -1,4 +1,7 @@
-[
+
+// JSON DATA
+
+const profileJSON = [
   {
     "name": "Paolo Maldini",
     "jobTitle": "Front End Developer",
@@ -51,7 +54,59 @@
        "HTML", "CSS", "JavaScript", "React", "Vue", "Redux"
     ]
   }
- ]
+];
+
+// Convert JSON to object
+const profileString = JSON.stringify(profileJSON);
+const profileInfoBank = JSON.parse(profileString);
+
+// Creates Individual Cards
+profileInfoBank.forEach(function(currentUser){
+
+  for(const key in currentUser){
+
+    //Sets the base card
+    let output ='<div class ="card-container">'
+
+    //Creates Profile Base Div
+    let profileBase = `<div class ="profile-base"><img src="img/headshot.jpg" alt="Dashing portrait of Paolo Maldini"><h2>${currentUser.name}</h2><p>${currentUser.jobTitle}</p></div>`;
+
+    //Creates Profile Deets Div
+    let profileDeets = `<div class="profile-deets">`;
+
+    let jobDeets = `<div class ="company"><h3>Company: </h3><p>${currentUser.company}</p></div>`;
+    let expDeets = `<div class ="experience"><h3>Experience: </h3><p>${currentUser.experience}</p></div>`;
+    let schoolDeets = `<div class ="school"><h3>School: </h3><p>${currentUser.school}</p></div>`;
+    let majorDeets = `<div class ="experience"><h3>Major: </h3><p>${currentUser.major}</p></div>`;
+    let emailDeets = `<div class ="email"><h3>Email: </h3><p>${currentUser.email}</p></div>`
+    let linkedDeets = `<div class="linked"><img src="img/linkedin.svg" alt="LinkedInLogo"><p>${currentUser.linkedInUrl}</p></div>`
+
+    output = output + profileBase + profileDeets + jobDeets + expDeets + schoolDeets + majorDeets + emailDeets + linkedDeets +'</div>' + '</div>';
+
+    //Outputs to Page
+    document.querySelector('.template-hook').innerHTML = output;
+  }
+})
 
 
- console.log('I passed throughs')
+
+// // EXPERIMENT START Trying to see if I can better automate the deets sectiong cuz it repeats
+//
+// function shellMaker(filling){
+//   let shell = `<div class ="${filling}<h3>${filling}</h3><p>${filling}<p></div>"`
+// }
+//
+// function deetConstructor(company, experience, school, major, email){
+//   // maybe we don't need this because we are not making a new object?
+//   this.company = company;
+//   this.experience = experience;
+//   this.school = school;
+//   this.major = major;
+//   this.email = email;
+// }
+//
+// const dummy = new deetConstructor(currentUser.company, currentUser.experience, currentUser.school, currentUser.major, currentUser.email);
+// console.log(dummy)
+// //EXPERIMENT END
+
+// profileDeets = profileDeets + `</div>`;
