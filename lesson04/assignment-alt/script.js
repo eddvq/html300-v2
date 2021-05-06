68f765f366b5ab4d9a50a952713b4b4e0882f355
eddy vazquez <>
2021-05-05 21:11:37 -0700

    Sliding menu toggles

--- lesson04/assignment-alt/script.js
+++ lesson04/assignment-alt/script.js
@@ -1,83 +1,14 @@
 
-// JSON DATA
+const navMenuIcon = document.querySelector('.Nav__menu');
+const navMenuCloseIcon = document.querySelector('.Nav__sliding-menu__ul__close-icon');
 
-const profileJSON = [
-  {
-    "name": "Paolo Maldini",
-    "jobTitle": "Front End Developer",
-    "company": "Web Weavers",
-    "experience": "3 years",
-    "school": "University of Washington",
-    "major": "Marketing",
-    "email": "paolo@example.com",
-    "linkedInUrl": "paolo.linkedinprofile.com",
-    "codeLanguages": [
-        "HTML", "CSS", "JavaScript", "Node", "Express"
-    ]
-  },
-  {
-    "name": "Barbara Bonansea",
-    "jobTitle": "Software Engineer",
-    "company": "Excellence in the Cloud",
-    "experience": "12 years",
-    "school": "University of Southern California",
-    "major": "Computer Science",
-    "email": "barbara@example.com",
-    "linkedInUrl": "barbara.linkedinprofile.com",
-    "codeLanguages": [
-        "HTML", "JavaScript", "C", "Go"
-    ]
-  },
-  {
-    "name": "Javier Hernandez",
-    "jobTitle": "User Experience Engineer",
-    "company": "Web Sites and More",
-    "experience": "5 years",
-    "school": "Seattle University",
-    "major": "Performing Arts",
-    "email": "javier@example.com",
-    "linkedInUrl": "javier.linkedinprofile.com",
-    "codeLanguages": [
-        "HTML", "CSS"
-    ]
-  },
-  {
-    "name": "Maribel Dominguez",
-    "jobTitle": "Front End Engineer",
-    "company": "Bits and Bytes",
-    "experience": "6 years",
-    "school": "University of Washington",
-    "major": "Mechanical Engineering",
-    "email": "maribel@example.com",
-    "linkedInUrl": "maribel.linkedinprofile.com",
-    "codeLanguages": [
-       "HTML", "CSS", "JavaScript", "React", "Vue", "Redux"
-    ]
-  }
-];
 
-// Convert JSON to object
-const profileString = JSON.stringify(profileJSON);
-const profileInfoBank = JSON.parse(profileString);
+// Toggles the sliding navigation bar on mobile
+navMenuIcon.addEventListener('click', function(){
+  document.querySelector('.Nav__sliding-menu').style.display = "block";
+});
 
-let output = "";
+navMenuCloseIcon.addEventListener('click', function(){
+  document.querySelector('.Nav__sliding-menu').style.display = "none";
 
-// Creates Individual Cards
-profileInfoBank.forEach(function(currentUser){
-    //Creates Profile Base Div
-    let profileBase = `<div class ="card-container"><div class ="profile-base"><img src="img/headshot.jpg" alt="Dashing portrait of ${currentUser.name}"><h2>${currentUser.name}</h2><p>${currentUser.jobTitle}</p></div>`;
-
-    //Creates Profile Deets Div
-    let profileDeets = `<div class="profile-deets">`;
-
-    let jobDeets = `<div class ="company"><h3>Company: </h3><p>${currentUser.company}</p></div>`;
-    let expDeets = `<div class ="experience"><h3>Experience: </h3><p>${currentUser.experience}</p></div>`;
-    let schoolDeets = `<div class ="school"><h3>School: </h3><p>${currentUser.school}</p></div>`;
-    let majorDeets = `<div class ="experience"><h3>Major: </h3><p>${currentUser.major}</p></div>`;
-    let emailDeets = `<div class ="email"><h3>Email: </h3><p>${currentUser.email}</p></div>`
-    let linkedDeets = `<div class="linked"><img src="img/linkedin.svg" alt="LinkedInLogo"><p>${currentUser.linkedInUrl}</p></div>`
-
-    output = output + profileBase + profileDeets + jobDeets + expDeets + schoolDeets + majorDeets + emailDeets + linkedDeets +'</div>' + '</div>';
-
-    document.querySelector('.template-hook').innerHTML = output;
 })
